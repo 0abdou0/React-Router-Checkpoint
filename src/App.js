c8667afe2a68cca19f1bacf3fb53a9a6d39c7d2e
0abdou0 <abdallaoui.abderrahmane.01@gmail.com>
2021-04-25 18:41:45 +0100

    R Router Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Popular from "./component/Popular";
+import Top from "./component/Top";
+import Description from "./component/Description";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Router>
+        <nav
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <button style={{ color: "blue", fontSize: 25 }}>
+            <Link to="/popular">popular</Link>
+          </button>
+          <button style={{ color: "blue", fontSize: 25 }}>
+            <Link to="/top">top</Link>
+          </button>
+        </nav>
+        <Switch>
+          <Route exact path="/popular">
+            <Popular />
+          </Route>
+          <Route exact path="/top">
+            <Top />
+          </Route>
+          <Route exact path="/details/:name">
+            <Description />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
